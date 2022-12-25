c79668c169056897d17d826098e1befb8862b573
fares elgohary <fareselgohary32@gmail.com>
2022-12-25 03:58:47 +0200

    faresn

--- ha.js
+++ ha.js
@@ -1,3 +1,12 @@
+const { response } = require('express');
 const express = require('express');
 var app = express();
+const path = require('path')
+app.use(express.static(__dirname + '/public'))
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/public/ha.html")
+})
+app.get('/home', (req, res) => {
+    res.render('home')
+})
 app.listen(3306, () => console.log("listening.."))
