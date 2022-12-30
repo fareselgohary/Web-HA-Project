ad6d6b3295e2ea5149db5aef0db35cdff492e7b1
fares elgohary <fareselgohary32@gmail.com>
2022-12-30 18:50:57 +0200

    sd

--- fares.js
+++ fares.js
@@ -5,9 +5,10 @@ const conn = mysql.createConnection({
     password: "7460",
     database: "web_15",
 })
-conn.query(`SELECT * FROM `log` WHERE 1`, function (err, result, fields) {
+conn.query(`select * from log where usern=? and userp=?`, function (err, result, fields) {
     if (err) {
         return console.log(err);
     }
-    return console.log(result);
+    if (usen =)
+        return console.log(result);
 })
