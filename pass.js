694280f41a283e47d0274fa7ecf6552f6022b847
fares elgohary <fareselgohary32@gmail.com>
2022-12-31 20:26:50 +0200

    fasfa

--- pass.js
+++
@@ -1,9 +0,0 @@
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy
-const user = require('./models/db')
-
-
-passport.use(new LocalStrategy(function verify(username, password, cb) {
-    db.get('SELECT * FROM users WHERE username = ?', [username], function (err, user) {
-        if (err) { return cb(err); }
-        if (!user) { return cb(null, false, { message: 'Incorrect username or password.' }); }
